35aa84e8ffacb3079180317b458eaa5130c85e6b
Ben Fedoruk <fedorukben@gmail.com>
2023-07-27 17:57:14 -0400

    dmenu script work and got rid of patch diffs

--- config.h
+++ config.h
@@ -12,8 +12,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#7e705a", "#3b3228" },
+	[SchemeSel] = { "#7e705a", "#4d4134" },
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
